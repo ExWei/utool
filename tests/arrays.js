f73617bd01e41d59c6742c5114889d43a7106098
Alex Schafer <aexwei@gmail.com>
2014-07-03 23:27:02 +0400

    Added tests for array#first

--- tests/arrays.js
+++ tests/arrays.js
@@ -4,6 +4,12 @@ var u = require("../lib/index.js");
 
 describe("Arrays", function () {
     describe("#last()", function () {
+        it("Should return first element in array", function () {
+            u.last([1, 2, 3]).should.be.equal(1);
+            u.last(["a", "b", "c"]).should.be.equal("a");
+            u.last(["d"]).should.be.equal("d");
+            u.last([]).should.be.equal(undefined);
+        });
         it("Should return last element in array", function () {
             u.last([1, 2, 3]).should.be.equal(3);
             u.last(["a", "b", "c"]).should.be.equal("c");
