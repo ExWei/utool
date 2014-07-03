5b9b4f7a77660fcb72f8f2b02f42584a1e640cc5
Alex Schafer <aexwei@gmail.com>
2014-07-03 23:26:34 +0400

    Added tests for array#last

--- tests/arrays.js
+++ tests/arrays.js
@@ -3,4 +3,12 @@ var should = require('should');
 var u = require("../lib/index.js");
 
 describe("Arrays", function () {
+    describe("#last()", function () {
+        it("Should return last element in array", function () {
+            u.last([1, 2, 3]).should.be.equal(3);
+            u.last(["a", "b", "c"]).should.be.equal("c");
+            u.last(["d"]).should.be.equal("d");
+            u.last([]).should.be.equal(undefined);
+        });
+    });
 });
