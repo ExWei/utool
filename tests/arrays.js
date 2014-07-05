537fb65724516561e4243a85421ced4cfbd1439a
Alex Schafer <aexwei@gmail.com>
2014-07-05 23:28:42 +0400

    fixed array#first tests

--- tests/arrays.js
+++ tests/arrays.js
@@ -5,10 +5,10 @@ var u = require("../lib/index.js");
 describe("Arrays", function () {
     describe("#last()", function () {
         it("Should return first element in array", function () {
-            u.last([1, 2, 3]).should.be.equal(1);
-            u.last(["a", "b", "c"]).should.be.equal("a");
-            u.last(["d"]).should.be.equal("d");
-            u.last([]).should.be.equal(undefined);
+            u.first([1, 2, 3]).should.be.equal(1);
+            u.first(["a", "b", "c"]).should.be.equal("a");
+            u.first(["d"]).should.be.equal("d");
+            u.first([]).should.be.equal(undefined);
         });
         it("Should return last element in array", function () {
             u.last([1, 2, 3]).should.be.equal(3);
