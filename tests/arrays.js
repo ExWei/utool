b68acd19683c94b27b8f17acd71a931112c9be70
Alex Schafer <aexwei@gmail.com>
2014-07-06 23:36:50 +0400

    fixed structure of tests for arrays

--- tests/arrays.js
+++ tests/arrays.js
@@ -3,25 +3,31 @@ var should = require('should');
 var u = require("../lib/index.js");
 
 describe("Arrays", function () {
-    describe("#last()", function () {
+    describe("#first()", function () {
         it("Should return first element in array", function () {
             u.first([1, 2, 3]).should.be.equal(1);
             u.first(["a", "b", "c"]).should.be.equal("a");
             u.first(["d"]).should.be.equal("d");
             u.first([]).should.be.equal(undefined);
         });
+    });
+    describe("#last()", function () {
         it("Should return last element in array", function () {
             u.last([1, 2, 3]).should.be.equal(3);
             u.last(["a", "b", "c"]).should.be.equal("c");
             u.last(["d"]).should.be.equal("d");
             u.last([]).should.be.equal(undefined);
         });
+    });
+    describe("#reverse()", function () {
         it("Should return reversed array", function () {
             u.reverse([1, 2, 3]).should.be.eql([3, 2, 1]);
             u.reverse(["a", "b", "c"]).should.be.eql(["c", "b", "a"]);
             u.reverse(["d"]).should.be.eql(["d"]);
             u.reverse([]).should.be.equal([]);
         });
+    });
+    describe("#rest()", function () {
         it("Should rest of array array", function () {
             u.rest([1, 2, 3]).should.be.eql([2, 3]);
             u.rest([1, 2, 3], 2).should.be.eql([3]);
